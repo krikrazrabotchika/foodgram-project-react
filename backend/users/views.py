8c36840ef4aa1c762ba3f2537fbbc4684ee5066d
Павел Поздняков <pas2003poz@yandex.ru>
2022-12-25 16:02:56 +0300

    6

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,3 @@
-from api.pagination import CustomPagination
-from api.serializers import CustomUserSerializer, SubscribeSerializer
-from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from rest_framework import status
@@ -8,9 +5,10 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from api.pagination import CustomPagination
+from api.serializers import CustomUserSerializer, SubscribeSerializer
+from recipes.models import User
 from .models import Subscribe
-
-User = get_user_model()
 
 
 class CustomUserViewSet(UserViewSet):
