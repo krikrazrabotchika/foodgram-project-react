8f4427471f628c58c232350a356de1e27e7fb19e
Павел Поздняков <pas2003poz@yandex.ru>
2023-01-09 19:16:08 +0300

    homie

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -12,18 +12,9 @@ class IngredientAdmin(admin.ModelAdmin):
     list_filter = ('name',)
 
 
-class IngredientsRecipesInline(admin.TabularInline):
-    model = Recipe
-
-
-class RecipesTagsInline(admin.TabularInline):
-    model = Tag
-
-
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'count_favorites')
     list_filter = ('author', 'name', 'tags')
-    inlines = [IngredientsRecipesInline, RecipesTagsInline]
 
     def count_favorites(self, obj):
         return obj.favorites.count()
