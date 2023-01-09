d8df6589e539e48ffead27f93fa511d07e5189db
Павел Поздняков <pas2003poz@yandex.ru>
2023-01-09 19:25:12 +0300

    pashtet77

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -5,16 +5,19 @@ from .models import Cart, Favorite, Ingredient, Recipe, Tag
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'color')
+    model = Tag
 
 
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     list_filter = ('name',)
+    model = Ingredient
 
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'count_favorites')
     list_filter = ('author', 'name', 'tags')
+    inlines = [TagAdmin, IngredientAdmin]
 
     def count_favorites(self, obj):
         return obj.favorites.count()
