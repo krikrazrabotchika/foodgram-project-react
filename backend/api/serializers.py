0a86fccdcb0ebdd57f9c72151fbe681273aa178a
Павел Поздняков <pas2003poz@yandex.ru>
2023-01-11 15:03:30 +0300

    wow

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -155,8 +155,8 @@ class FollowSerializer(serializers.ModelSerializer):
         request = self.context.get('request').data
         limit = request.get('recipes_limit')
         recipes = (
-            obj.author.recipe.all()[:int(limit)] if limit
-            else obj.author.recipe.all())
+            obj.username.recipe.all()[:int(limit)] if limit
+            else obj.username.recipe.all())
         return RecipeSubscribeSerializer(
             recipes,
             many=True).data
